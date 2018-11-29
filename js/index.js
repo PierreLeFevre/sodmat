2b9849b2858c86f96cb23ccafa0c9eb29575deab
Pierre <pierre.lefevre@elev.it-gymnasiet.se>
2018-11-29 22:19:31 +0100

    polished

--- js/index.js
+++ js/index.js
@@ -1,19 +1,42 @@
 var food = {
-	week: 48,
-	food: {
-		monday: {
+	48: {
+		1: {
 			normal: "Spaghetti med köttfärssås",
 			vego: "Spaghetti med tofusås",
 			extra: "Kycklingklubbor"
 		},
 
-		tuesday: {
+		2: {
 			normal: "Hamburgare med ketchup",
 			vego: "Quornburgare med ketchup",
 			extra: "Kycklingklubbor"
+		},
+
+		3: {
+			normal: "Spaghetti med köttfärssås",
+			vego: "Spaghetti med tofusås",
+			extra: "Kycklingklubbor"
+		},
+
+		4: {
+			normal: "Hamburgare med ketchup",
+			vego: "Quornburgare med ketchup",
+			extra: "Kycklingklubbor"
+		},
+
+		5: {
+			normal: "Spaghetti med köttfärssås",
+			vego: "Spaghetti med tofusås",
+			extra: "Kycklingklubbor"
 		}
-	}	
+	},
 };
+
+// get week
+Date.prototype.getWeek = function(){
+	        var onejan = new Date(this.getFullYear(), 0, 1);
+	        return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+	    }
 
 //cookie handling functions
 
@@ -58,8 +81,24 @@ $(window).on("load", function(){
 		$('.info').show();
 	}
 
-	$(".navbar").on("click", function(){
-		alert(food.food.monday.normal);
-	});
+	for (var i = 1; i <= 5; i++) {
+
+		var days = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag'];
+
+		$(".foodScroller").append('<div class="day day-' + i + '"><h1 class="dayHeader">' + days[i-1] + '</h1></div>');
+
+		$(".day-" + i).append('<p class="course course-normal"><span class="type type-normal">Dagens rätt: </span>' + (food[new Date().getWeek()][i].normal) + '</p>');
+		$(".day-" + i).append('<p class="course course-vego"><span class="type type-vego">Vegetariskt: </span>' + (food[new Date().getWeek()][i].vego) + '</p>');
+		$(".day-" + i).append('<p class="course course-extra"><span class="type type-extra">Extrarätt: </span>' + (food[new Date().getWeek()][i].extra) + '</p>');		
+
+	};
+
+	$(".desktopSchema").height( ( $(window).height() ) - 50 - ( $(".foodContainer").height() ) );
+
+});
+
+$( window ).resize(function() {
+  
+	$(".desktopSchema").height( ( $(window).height() ) - 50 - ( $(".foodContainer").height() ) );
 
 });
