a58393def82ec3bbddbfae3ed198ccf6b17f771a
Pierre <pierre.lefevre@elev.it-gymnasiet.se>
2018-12-03 10:21:37 +0100

    added food for rest of year

--- js/index.js
+++ js/index.js
@@ -25,6 +25,84 @@ var food = {
 			vego: "Vegokorv i tomatsås serveras med potatismos"
 		}
 	},
+	49: {
+		1: {
+			normal: "Köttbullar med makaroner och KRAV-Ketchup",
+			vego: "Morotsbullar med makaroner och KRAV-Ketchup"
+		},
+
+		2: {
+			normal: "Kebab med pitabröd & tillbehör",
+			vego: "Kebabkryddad Pulled Vego med pitabröd & tillbehör"
+		},
+
+		3: {
+			normal: "Pannbiff med BBQ-sås samt ris",
+			vego: "Vegobiff med BBQ-sås samt ris"
+		},
+
+		4: {
+			normal: "Kyckling i curry serveras med pasta",
+			vego: "Grönsaker i curry serveras med pasta"
+		},
+
+		5: {
+			normal: "Taco serveras med nachos, salsa & ris",
+			vego: "Taco serveras med nachos, salsa & ris"
+		}
+	},
+	50: {
+		1: {
+			normal: "Italiensk nötfärsbiff med basilikasås & potatis",
+			vego: "Rödbetsbiff med basilikasås & potatis"
+		},
+
+		2: {
+			normal: "Kebabgryta med ris",
+			vego: "Kebabgryta med sojastripes & grönsaker"
+		},
+
+		3: {
+			normal: "Panerad fiskfilé med kall örtsås och potatis",
+			vego: "Blomkål & ostbiffar med kall örtsås och potatis"
+		},
+
+		4: {
+			normal: "Julbord",
+			vego: "Julbord"
+		},
+
+		5: {
+			normal: "Mammas spaghetti med körfärssås och KRAV-Ketchup",
+			vego: "Mammas spaghetti med sojafärssås och KRAV-Ketchup"
+		}
+	},
+	51: {
+		1: {
+			normal: "Hamburgare med bröd, dressing, sallad & klyftpotatis",
+			vego: "Greenburgare med bröd, dressing, sallad & klyftpotatis"
+		},
+
+		2: {
+			normal: "Panerad torsk med kall dillsås & potatis",
+			vego: "Stekt quornfilé med kall dillsås & potatis"
+		},
+
+		3: {
+			normal: "(Julavslutning) Korv Strogranoff med ris",
+			vego: "(Julavslutning) Korv Strogranoff på soja med ris"
+		},
+
+		4: {
+			normal: "(Studiedag) Grekisk köttfärsröra serveras med pasta & aioli",
+			vego: "(Studiedag) Grekisk sojafärsröra serveras med pasta & aioli"
+		},
+
+		5: {
+			normal: "(Jullov) Skolan stängd.",
+			vego: "(Jullov) Skolan stängd."
+		}
+	},
 };
 
 // get week
