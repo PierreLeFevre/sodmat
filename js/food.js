4019065f0a78661768ca50a25e4d24cbb4b87305
Pierre <pierre.lefevre@elev.it-gymnasiet.se>
2019-03-26 10:14:36 +0100

    updated food

--- js/food.js
+++ js/food.js
@@ -1,30 +1,4 @@
 var food = {
-	12: {
-		1: {
-			normal: "Farmarbiff med potatismos & lingonsylt",
-			vego: "Grönkålsbiff med potatismos & lingonsylt"
-		},
-
-		2: {
-			normal: "Tandoorikryddad kyckling & grönsaker serveras med ris & yoghurtsås",
-			vego: "Tandoori kryddad tofu & grönsaker serveras med ris & yoghurtsås"
-		},
-
-		3: {
-			normal: "Panerad fisk med kall dillsås & potatis",
-			vego: "Stekt quorn med kall dillsås & potatis"
-		},
-
-		4: {
-			normal: "Mammas spaghetti med köttfärssås bästa KRAV-ketchupen",
-			vego: "Mammas spaghetti med sojafärssås bästa KRAV-ketchupen"
-		},
-
-		5: {
-			normal: "Hamburgare med bröd, sallad, dressing & klyftpotatis",
-			vego: "Green burger med bröd, sallad, dressing & klyftpotatis"
-		}
-	},
 	13: {
 		1: {
 			normal: "Köttbullar med gräddsås, potatis & inlagd gurka",
@@ -49,7 +23,34 @@ var food = {
 		5: {
 			normal: "Pulled Pork med bröd, rökig barbecuesås samt ris",
 			vego: "Pulled Vego med bröd, rökig barbecuesås samt ris",
-			extre: "Pulled Chicken med bröd, rökig barbecuesås samt ris"
+			extra: "Pulled Chicken med bröd, rökig barbecuesås samt ris"
+		}
+	},
+	14: {
+		1: {
+			normal: "Fläskschnitzel med gröna ärtor, klyftpotatis & skysås",
+			vego: "Vegetarisk schnitzel med gröna ärtor, klyftpotatis & skysås",
+			extra: "Kycklingschnitzel med gröna ärtor, klyftpotatis & skysås"
+		},
+
+		2: {
+			normal: "Nötfärsgryta med majs & champinjoner serveras med ris",
+			vego: "Grönsaksgryta med mango, lime & blomkål samt ris"
+		},
+
+		3: {
+			normal: "Tomatsoppa med mjukt bröd & pålägg",
+			vego: "Tomatsoppa med mjukt bröd & pålägg"
+		},
+
+		4: {
+			normal: "Pepparbiff med champinjonsås & kokt potatis",
+			vego: "Morotbiff med kall citronsås & kokt potatis"
+		},
+
+		5: {
+			normal: "Kycklingwok med ris",
+			vego: "Grönsakswok med ris"
 		}
 	}
 };
