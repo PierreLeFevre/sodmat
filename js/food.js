3d676dfafb1824a60fbd77cb40b8f34f30829270
Pierre Le Fevre <pierrelefevre@Pierres-MacBook-Pro.local>
2019-04-09 10:59:02 +0200

    added påsk food

--- js/food.js
+++ js/food.js
@@ -1,58 +1,4 @@
 var food = {
-	13: {
-		1: {
-			normal: "Köttbullar med gräddsås, potatis & inlagd gurka",
-			vego: "Falafel med gräddsås, potatis & inlagd gurka"
-		},
-
-		2: {
-			normal: "Pasta Alfredo med kyckling, ost & gröna ärtor",
-			vego: "Pasta Alfredo med quorn, ost & gröna ärtor"
-		},
-
-		3: {
-			normal: "Laxbiff med curryremoulad & ris",
-			vego: "Grönsaksbiff Caribean med curryremoulad & ris"
-		},
-
-		4: {
-			normal: "Korv Stroganoff serveras med ris",
-			vego: "Dhal, indisk linsgryta serveras med ris"
-		},
-
-		5: {
-			normal: "Pulled Pork med bröd, rökig barbecuesås samt ris",
-			vego: "Pulled Vego med bröd, rökig barbecuesås samt ris",
-			extra: "Pulled Chicken med bröd, rökig barbecuesås samt ris"
-		}
-	},
-	14: {
-		1: {
-			normal: "Fläskschnitzel med gröna ärtor, klyftpotatis & skysås",
-			vego: "Vegetarisk schnitzel med gröna ärtor, klyftpotatis & skysås",
-			extra: "Kycklingschnitzel med gröna ärtor, klyftpotatis & skysås"
-		},
-
-		2: {
-			normal: "Nötfärsgryta med majs & champinjoner serveras med ris",
-			vego: "Grönsaksgryta med mango, lime & blomkål samt ris"
-		},
-
-		3: {
-			normal: "Tomatsoppa med mjukt bröd & pålägg",
-			vego: "Tomatsoppa med mjukt bröd & pålägg"
-		},
-
-		4: {
-			normal: "Pepparbiff med champinjonsås & kokt potatis",
-			vego: "Morotbiff med kall citronsås & kokt potatis"
-		},
-
-		5: {
-			normal: "Kycklingwok med ris",
-			vego: "Grönsakswok med ris"
-		}
-	},
 	15: {
 		1: {
 			normal: "Kycklingköttbullar med tomatsås & pasta",
@@ -65,8 +11,8 @@ var food = {
 		},
 
 		3: {
-			normal: "Spagetti med köttfärssås & KRAV-ketchup",
-			vego: "Spagetti med sojafärssås & KRAV-ketchup"
+			normal: "Mammas spagetti med såsig köttfärs och bästa KRAV-ketchupen",
+			vego: "Syrrans spaghetti med pk-färssås & KRAV-ketchup"
 		},
 
 		4: {
@@ -78,6 +24,32 @@ var food = {
 		5: {
 			normal: "Kebab med pitabröd & tillbehör",
 			vego: "Kebabkryddade sojastrimlor med pitabröd & tillbehör"
+		}
+	},
+	16: {
+		1: {
+			normal: "Påskig måndagsmat",
+			vego: "Ett lagom gott alternativ"
+		},
+
+		2: {
+			normal: "Grillad påskhare",
+			vego: "Grillad chokladpåskhare utan mjölk"
+		},
+
+		3: {
+			normal: "Omelett på påskägg (äckligt tbh..)",
+			vego: "Omelett utan ägg. Luftig substans."
+		},
+
+		4: {
+			normal: "Sönderköttad välfärdslagande smet",
+			vego: "Leffes specialare"
+		},
+
+		5: {
+			normal: "Det är fredag mina kamrater",
+			vego: "Haha aa deere"
 		}
 	}
 };
