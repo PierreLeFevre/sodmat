455650291e729158ed94f336b5a68f38f60a2f3a
Beppsi <46752177+Beppsi@users.noreply.github.com>
2019-09-15 18:18:59 +0200

    fixed spelling error

--- js/food.js
+++ js/food.js
@@ -1,34 +1,5 @@
 var food = {
 
-	37: {
-		1: {
-			normal: "Pasta med skink & ostsås",
-			vego: "Pasta med lins & broccolisåss",
-			extra: "Pasta med Kalkon & ostsås"
-		},
-
-		2: {
-			normal: "Panerad torsk med kall dillsås & potatis",
-			vego: "Vegetarisk biff med kall dillsås & potatis"
-		},
-
-		3: {
-			normal: "Kycklingwok med ris",
-			vego: "Grönsakswok med ris"
-		},
-
-		4: {
-			normal: "Pannbiff med ajvarkräm & klyftpotatis",
-			vego: "Stekt quorn med ajvarkräm & klyftpotatis"
-		},
-
-		5: {
-			normal: "Chili cos carne serveras med ris",
-			vego: "Chili sin carne serveras med ris",
-			extra: "Chili tan carne serveras med ris"
-			
-		}
-	},
 	38: {
 		1: {
 			normal: "Köttbullar med makaroner & KRAV-ketchup",
@@ -36,7 +7,7 @@ var food = {
 		},
 
 		2: {
-			normal: "Korv Stroganoff serveras med ri",
+			normal: "Korv Stroganoff serveras med ris",
 			vego: "Vegetarisk Stroganoff serveras med ris"
 		},
 
@@ -56,6 +27,33 @@ var food = {
 		}
 	},
 	39: {
+		1: {
+			normal: " ",
+			vego: " "
+		},
+
+		2: {
+			normal: " ",
+			vego: " "
+		},
+
+		3: {
+			normal: " ",
+			vego: " ",
+			extra: " "
+		},
+
+		4: {
+			normal: " ",
+			vego: " "
+		},
+
+		5: {
+			normal: " ",
+			vego: " "
+		}
+	},
+	40: {
 		1: {
 			normal: " ",
 			vego: " "
