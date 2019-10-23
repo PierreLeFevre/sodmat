011a0e77515600409346adfd2ce76d88cf65aa5c
Beppsi <46752177+Beppsi@users.noreply.github.com>
2019-10-23 11:16:03 +0200

    43, 45

--- js/food.js
+++ js/food.js
@@ -1,119 +1,66 @@
 var food = {
 
-	39: {
-		1: {
-			normal: "Oxpytt serveras med KRAV-rödbetor",
-			vego: "Veggiepytt serveras med vitlöksdressing"
-		},
-
-		2: {
-			normal: "Pasta Carbonara",
-			vego: "Pasta med ratatouille",
-			extra: "Kalkon Carbonara" 
-		},
-
-		3: {
-			normal: "Kebabgryta serveras med ris",
-			vego: "Kebabkryddade sojastrimlor & grönsaker med ris"
-			
-		},
-
-		4: {
-			normal: "Torskburgare med potatismos & gurkmajonnäs",
-			vego: "Vegetarisk Pad Thai med tofu"
-		},
-
-		5: {
-			normal: "Chicken nuggets serveras med sweet chili & ris",
-			vego: "Vegetarisk schnitzel serveras med sweet chili & ris"
-		}
-	},
-	
-	40: {
-		1: {
-			normal: "Hamburgare med bröd, sallad, dressing & klyftpotatis",
-			vego: "Green Burger med bröd, sallad, dressing & klyftpotatis"
-		},
-
-		2: {
-			normal: "Ugnsstekt falukörv med potatismos & KRAV-ketchup",
-			vego: "Ugnsstekt sojakorv med potatismos & KRAV-ketchup"
-		},
-
-		3: {
-			normal: "Pepparbiff med bearnaisesås & klyftpotatis",
-			vego: "Caribean biff med bearnaisesås & klyftpotatis"
-			
-		},
-
-		4: {
-			normal: "Pulled Chicken med bröd, rökig BBQ-sås & ris",
-			vego: "Pulled Vego med bröd, rökig BBQ-sås & ris"
-		},
-
-		5: {
-			normal: "Kycklingköttbullar med pasta & tomatsås",
-			vego: "Falafel med pasta & tomatsås"
-		}
-	},
-	
-	41: {
-		1: {
-			normal: "Tacobiff serveras med chipotlekräm & ris",
-			vego: "Provensalsk gryta med edemame bönor & ratatouille serveras med ris"
-		},
-
-		2: {
-			normal: "Panerad fisk med kall örtsås och potatis",
-			vego: "Vegobiff med kall örtsås och potatis"
-		},
-
-		3: {
-			normal: "Thaigryta med kyckling, kokos, chili och ingefära serveras med ris",
-			vego: "Thaigryta med grönsaker, kokos, chili och ingefära serveras med ris"
-			
-		},
-
-		4: {
-			normal: "Spagetti med köttfärssås",
-			vego: "Spagetti med sojafärssås"
-		},
-
-		5: {
-			normal: "Kycklingschnitzel serveras med dragonkräm & ris",
-			vego: "Broccoligratäng toppad med tofu"
-		}
-	},
-	42: {
-		1: {
-			normal: "Oxjärpar med gräddsås, potatis & lingon",
-			vego: "Morotsbullar med gräddsås, potatis & lingon"
-		},
-
-		2: {
-			normal: "Pasta med kyckling & currysås",
-			vego: "Pasta med quorn & currysås"
-		},
-
-		3: {
-			normal: "Kebab med pitabröd & tillbehör",
-			vego: "Kebabkryddade sojastrimlor med pitabröd & tillbehör"
-			
-		},
-
-		4: {
-			normal: "Nudelsoppa med kyckling serveras med bröd & pålägg",
-			vego: "Nudelsoppa med grönsaker serveras med bröd & pålägg"
-		},
-
-		5: {
-			normal: "Taco på nötfärs serveras med nachos, ris & salsa",
-			vego: "Taco på sojafärs serveras med nachos, ris & salsa"
-		}
-	},
 	43: {
 		1: {
-			normal: "Mat, tror jag",
+			normal: "Sprödbakad torsk med kall dillsås & potati",
+			vego: "Vegetarisk biff med kall dillsås & potatis"
+		},
+
+		2: {
+			normal: "Korv Stroganoff serveras med ris",
+			vego: "Vegetarisk Stroganoff serveras med ris"
+		},
+
+		3: {
+			normal: "Köttbullar med potatismos & lingonsylt",
+			vego: "Falafel med myntasås, hummus & pitabröd",
+			
+		},
+
+		4: {
+			normal: "Chili con Pollo serveras med ris & riven ost",
+			vego: "Chili con veggie serveras med ris & riven ost"
+		},
+
+		5: {
+			normal: "Pasta med skink & ostsås/FF Kalkonsås",
+			vego: "Soja- & limemarinerad tofu med nudelwok & dressing"
+		}
+	},
+	
+	
+	45: {
+		1: {
+			normal: "Torskburgare med remouladsås & kokt potatis",
+			vego: "Vegetarisk schnitzel med remouladsås & kokt potatis"
+		},
+
+		2: {
+			normal: "Kebabrisotto med vitlöksdressing",
+			vego: "Grönsaksrisotto med linser & bönor serveras med vitlöksdressing"
+		},
+
+		3: {
+			normal: "Asiatisk kyckling i sötsur sås serveras med ris",
+			vego: "Moussaka (sojafärs, spenat, potatis, zucchini & fetaost)"
+			
+		},
+
+		4: {
+			normal: "Hamburgare med bröd, dressing, sallad & klyftpotatis",
+			vego: "Green burger med bröd, dressing, sallad & klyftpotatis"
+		},
+
+		5: {
+			normal: "Spagetti Bolognese",
+			vego: "Spagetti med vegetarisk Bolognese"
+		}
+	},
+	
+	
+	46: {
+		1: {
+			normal: "",
 			vego: " "
 		},
 
