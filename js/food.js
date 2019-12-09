576be1e3810f161332de79076726a65d8941317f
Beppsi <46752177+Beppsi@users.noreply.github.com>
2019-12-09 11:47:46 +0100

    50 & 51

--- js/food.js
+++ js/food.js
@@ -1,143 +1,54 @@
 var food = {
-
-	46: {
-		1: {
-			normal: "Oxpytt serveras med rödbetor",
-			vego: "Veggiepytt serveras med vitlöksdressing"
-		},
-
-		2: {
-			normal: "Chili con carne med ris",
-			vego: "Chili sin carne "
-		},
-
-		3: {
-			normal: "Kycklingschnitzel serveras med kall dragonsås & klyftpotatis",
-			vego: "Morotsbiffar"
-			
-		},
-
-		4: {
-			normal: "Pasta Carbonara",
-			vego: "Provensalsk gryta med edemame bönor & ratatouille",
-			extra: "Kalkon Carbonara"
-		},
-
-		5: {
-			normal: "Kebabgryta serveras med ris",
-			vego: "Sojafärslasagne"
-		}
-	},
-	
-		
-	47: {
-		1: {
-			normal: "Kycklingköttbullar med pasta & tomatsås",
-			vego: "Linsgryta med rostad blomkål"
-		},
-
-		2: {
-			normal: "Ugnsstekt falukorv med potatismos",
-			vego: "Ugnsstekt sojakorv"
-		},
-
-		3: {
-			normal: "Sprödbakad fisk med kall dillsås & kokt potatis",
-			vego: "Stekt quorn"
-		},
-
-		4: {
-			normal: "Pulled Chicken med rökig BBQ-sås serveras med ris",
-			vego: "Pulled Vego"
-		},
-
-		5: {
-			normal: "Pannbiff med bearnaisesås & klyftpotatis",
-			vego: "Caribean biff med bearnaisesås"
-		}
-	},
-	
-		
-	48: {
-		1: {
-			normal: "Thaigryta med kyckling, kokos & chili serveras med ris",
-			vego: "Thaigryta med grönsaker"
-		},
-
-		2: {
-			normal: "Fläskschnitzel med örtkräm & potatis",
-			vego: "Vegetarisk schnitzel",
-			extra: "Chimken numgits"
-		},
-
-		3: {
-			normal: "Korv Stroganoff med ris",
-			vego: "Vegetarisk Stroganoff med ris"
-		},
-
-		4: {
-			normal: "Spagetti med köttfärssås",
-			vego: "Spagetti med sojafärssås"
-		},
-
-		5: {
-			normal: "Kebab med pitabröd & tillbehör",
-			vego: "Falafel"
-		}
-	},
-	
-	49: {
-		1: {
-			normal: "Köttbullar med gräddsås, potatis & lingon",
-			vego: "Morotsbullar"
-		},
-
-		2: {
-			normal: "Pasta med kyckling & currysås",
-			vego: "Pasta med ratatouille & tofu"
-		},
-
-		3: {
-			normal: "Pannbiff med tzatziki & klyftpotatis",
-			vego: "Spansk bondomelett (ägg, potatis, lök, grädde) toppas med grillade grönsaker"
-		},
-
-		4: {
-			normal: "Panerad fisk med remouladsås & potatis",
-			vego: "Grönsaksbiff med remouladsås & potatis"
-		},
-
-		5: {
-			normal: "Taco på nötfärs serveras med nachos, ris & salsa",
-			vego: "Taco på sojafärs"
-		}
-	},
 	
 	50: {
 		1: {
-			normal: "",
-			vego: ""
+			normal: "Hamburgare med bröd, dressing, sallad & klyftpotatis",
+			vego: "Green Burger med bröd"
 		},
 
 		2: {
-			normal: "",
-			vego: ""
+			normal: "Green Burger med bröd",
+			vego: "Moussaka (sojafärs, spenat, potatis, zucchini & fetaost)"
 		},
 
 		3: {
-			normal: "",
-			vego: "",
-			extra: ""
+			normal: "Kebabgryta med ris",
+			vego: "Vegetarisk Pad Thai med nudlar"
 		},
 
 		4: {
-			normal: "",
+			normal: "Tomatsoppa serveras med pasta, mjukt bröd & färskost",
+			vego: "Tomatsoppa serveras med pasta, mjukt bröd & färskost"
+		},
+
+		5: {
+			normal: "Kycklingschnitzel med potatisgratäng",
+			vego: "Vegetarisk schnitzel med potatisgratäng"
+		}
+	},
+	
+	51: {
+		1: {
+			normal: "Falukorv med potatismos ",
+			vego: "Soja korv med potatismos"
+		},
+
+		2: {
+			normal: "Jullunch ;)"
+		},
+
+		3: {
+			normal: "Pannkaker me sylt å grädde"
+		},
+
+		4: {
+			normal: "Spaget",
 			vego: ""
 		},
 
 		5: {
-			normal: "",
-			vego: ""
+			normal: "Chickin nuggits med currykräm & ris",
+			vego: "Kikärtsbiff med currykräm & ris"
 		}
 	},
 
